e507a933340545d5d1ef14f3ae2fbe35f454e1da
Colin Stroble <colinstroble@gmail.com>
2019-07-25 14:16:51 -0400

    PHP Scripts Finished

---
+++ DiscordWebhookClient/DiscordWebhookClient/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+
+namespace DiscordWebhookClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string ImageURl(string imageLocation)
+        {
+            string imageUrl = string.Empty;
+            using (var w = new WebClient())
+            {
+                string clientID = "1f9bc62318c10fc";
+                w.Headers.Add("Authorization", "Client-ID " + clientID);
+                var values = new NameValueCollection
+                {
+                     { "image", Convert.ToBase64String(File.ReadAllBytes(imageLocation)) }
+                 };
+
+                byte[] response = w.UploadValues("https://api.imgur.com/3/upload.xml", values);
+
+                var responseXml = XDocument.Load(new MemoryStream(response));
+                return imageUrl = (string)responseXml.Root.Element("link");
+
+            }
+        }
+         
+    }
+}
--- PHP Scripts/sendmessage.php
+++ PHP Scripts/sendmessage.php
@@ -1,7 +1,35 @@
 <?php 
-$curl = curl_init("https://discordapp.com/api/webhooks/603718705540759552/lh9Mn8RwO5M4OUaAANvV-E6xW7qUHICp16UoBEs3sW5yFF-jK80Ferb2nbPLYXMsb7tz"); 
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array("content" => "hello")));
+require_once __DIR__ . '/vendor/autoload.php';
+use \DiscordWebhooks\Client;
+use \DiscordWebhooks\Embed;
 
-echo curl_exec($curl);
+$a = "https://discordapp.com/api/webhooks/603718705540759552/lh9Mn8RwO5M4OUaAANvV-E6xW7qUHICp16UoBEs3sW5yFF-jK80Ferb2nbPLYXMsb7tz";
+$webUrl = $_GET['webUrl'];
+
+$name = $_GET['botName'];
+
+$af = "https://cdn.shopify.com/s/files/1/1227/1058/collections/90302cce-3a00-4b99-b9ed-c390b3e97a34_2048x2048.png?v=1529797212";
+$avatarurl = $_GET['avatar'];
+
+$webhook = new Client($webUrl);
+$embed = new Embed();
+
+
+
+// variables
+$embedDescription = $_GET['embedDescription'];
+$embedTitle = $_GET['embedTitle'];
+$message = $_GET['message'];
+$embedColor = $_GET['embedColor'];
+
+
+if(isset($_GET['embed']))
+{
+$embed->title($embedTitle);
+$embed->description($embedDescription);
+$embed->color($hex);
+$webhook->avatar($avatarurl)->username($name)->message($message)->embed($embed)->send();
+}
+
+
  ?>
